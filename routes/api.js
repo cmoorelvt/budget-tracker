d5e6bb20639b2364eb2111916848336d79832722
Christine Moore <cmoorelvt@gmail.com>
2021-01-08 11:37:02 -0500

    minor changes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
+"use strict";
+
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbTransaction => {res.json(dbTransaction);
     })
     .catch(err => {
       res.status(404).json(err);
@@ -13,8 +14,7 @@ router.post("/api/transaction", ({body}, res) => {
 
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbTransaction => {res.json(dbTransaction);
     })
     .catch(err => {
       res.status(404).json(err);
@@ -23,8 +23,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbTransaction => {res.json(dbTransaction);
     })
     .catch(err => {
       res.status(404).json(err);
